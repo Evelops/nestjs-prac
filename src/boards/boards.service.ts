7b90b40ada5fb8472a4134e0cbd0b5c3a26861d3
eomseung-yeol <co6747@naver.com>
2023-05-22 13:57:04 +0900

    Feat: 게시물 생성(Create) 서비스 메서드 정의

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import {v1 as uuid} from 'uuid';
 @Injectable()
 export class BoardsService {
     private boards: Board[] = [];
@@ -12,5 +12,20 @@ export class BoardsService {
     getAllBoards(): Board[] {
         return this.boards;
     }
-     
+
+    /**
+     * 게시물을 생성하는 service의 생성 메서드 정의.
+    */
+   createBoard(title: string, description: string) {
+    //title, description을 기반으로 게시물을 생성하고, default stauts 는 Public으로 선정 
+    const board: Board = {
+        id:uuid(), // 아직 별도로 db를 연결하지 않았기에, 게시글의 고유값을 부여할 때 uuid 모듈을 기반으로 정의 
+        title: title,
+        description: description,
+        status: BoardStatus.PUBLIC
+    }
+    // 새롭게 들어온 board 객체를 맨상단에 정의한 boards 배열의 인자값으로 넣어주고 리턴.
+    this.boards.push(board);
+    return board;
+   }
 }
