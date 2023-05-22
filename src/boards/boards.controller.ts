3fbfada8f5ffa2f9877a6749bedc4222eca566dd
eomseung-yeol <co6747@naver.com>
2023-05-22 13:46:23 +0900

    Feat: 게시물에 대한 model 정의, service 에서 정의된 모델을 리턴해주는 메서드의 타입 정의

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 @Controller('boards')
 export class BoardsController {
     // controller에 대해서 서비스에 대한 di 주입을 해주어야함. 
@@ -7,7 +8,7 @@ export class BoardsController {
 
     // 모든 게시물에 대한 핸들러를 가져오는 GET 메서드 
     @Get('/')
-    getAllBoard(){
+    getAllBoard(): Board[]{
         return this.boardsService.getAllBoards();
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-    private boards = ['test1','test2'];
+    private boards: Board[] = [];
 
-    // boards 배열에 들어있는 모든 값을 리턴 
-    getAllBoards() {
+    /**
+     * boards 배열에 들어있는 모든 값을 리턴 
+     * getAllBoards() 라는 메서드가 리턴되는 값을 다음과 같이 지정 할 수 있음.
+     * */ 
+    getAllBoards(): Board[] {
         return this.boards;
     }
      
